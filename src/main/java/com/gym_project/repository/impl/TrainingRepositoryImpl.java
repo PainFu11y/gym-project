dcd6682ec2448890df386bee76a7ecdc4e04a23e
Painfu11y <333vahan777@gmail.com>
2026-03-23 22:20:16 +0400

    fix: repository imports fix

--- src/main/java/com/gym_project/repository/impl/TrainingRepositoryImpl.java
+++ src/main/java/com/gym_project/repository/impl/TrainingRepositoryImpl.java
@@ -9,9 +9,9 @@ import com.gym_project.repository.TrainingRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.List;
 
