732fc7f75808f6071c549f3c243e16d6177fdb39
Painfu11y <333vahan777@gmail.com>
2026-03-26 09:59:52 +0400

    feat: entityManager import fix

--- src/test/java/com/gym_project/repository/impl/TrainingTypeRepositoryImplTest.java
+++ src/test/java/com/gym_project/repository/impl/TrainingTypeRepositoryImplTest.java
@@ -8,8 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
