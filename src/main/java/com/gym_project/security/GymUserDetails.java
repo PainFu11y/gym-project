cc8ea3f2073c7631123cb6765d689c3f2b73ca87
Painfu11y <333vahan777@gmail.com>
2026-04-01 13:31:01 +0400

    feat: user details

---
+++ src/main/java/com/gym_project/security/GymUserDetails.java
@@ -0,0 +1,58 @@
+package com.gym_project.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class GymUserDetails implements UserDetails {
+
+    private final String username;
+    private final String password;
+    @Getter
+    private final Role role;
+
+    public GymUserDetails(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.asAuthority()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
